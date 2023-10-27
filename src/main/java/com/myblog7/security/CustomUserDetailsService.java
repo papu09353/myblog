963260892b59d87182a3f6a0ab4d3b7e093ea452
papu09353 <papu09353@gmail.com>
2023-10-27 07:57:59 -0700

    version 1 of My blog consisting post & comment feature

---
+++ src/main/java/com/myblog7/security/CustomUserDetailsService.java
@@ -0,0 +1,43 @@
+package com.myblog7.security;
+
+import com.myblog7.entity.Role;
+import com.myblog7.entity.User;
+import com.myblog7.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+
+        private  UserRepository userRepository;
+
+        public CustomUserDetailsService(UserRepository userRepository){
+
+            this.userRepository = userRepository;
+        }
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        // Retrieve user information from your data source (e.g., database)
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail,usernameOrEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username or email:" + usernameOrEmail));
+
+        // Create and return a CustomUserDetails instance
+        return  new org.springframework.security.core.userdetails.User(user.getEmail(),
+        user.getPassword(), mapRolesToAuthorities(user.getRoles()));
+    }
+
+    private Collection< ? extends GrantedAuthority>mapRolesToAuthorities(Set<Role> roles){
+       return roles.stream().map(role -> new
+               SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/com/myblog7/service/CommentService.java
@@ -0,0 +1,16 @@
+package com.myblog7.service;
+
+import com.myblog7.payload.CommentDto;
+
+import java.util.List;
+
+public interface CommentService {
+    CommentDto createComment(long postId, CommentDto commentDto);
+    List<CommentDto> getCommentsByPostId(long postId);
+
+    CommentDto getCommentsById(long postId, long commentId);
+
+    List<CommentDto> getAllCommentsById();
+
+    void deleteCommentById(long postId, long commentId);
+}
